5ebe5617706cfde852be47dcc8a99ebdc0ccfc68
Samurai <soaresmatheus55@yahoo.com.br>
2021-06-05 14:48:44 -0300

    Questao 2 lista 3

---
+++ Arvore/Arvore/MinhaArvore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arvore
+{
+    class MinhaArvore
+    {
+        public MinhaArvore(MinhaArvore esquerda, int dados, MinhaArvore direita)
+        {
+            Esquerda = esquerda;
+            Dados = dados;
+            Direita = direita;
+        }
+
+        public MinhaArvore Esquerda{get; set;}
+        public int Dados { get; set; }
+        public MinhaArvore Direita { get; set; }
+    }
+}
---
+++ Arvore/Arvore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arvore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaArvore Arvore59 = new MinhaArvore(null, 59, null);
+            MinhaArvore Arvore103 = new MinhaArvore(null, 103, null);
+            MinhaArvore Arvore48 = new MinhaArvore(null, 48, null);
+            MinhaArvore Arvore33 = new MinhaArvore(null, 33, null);
+            MinhaArvore Arvore51 = new MinhaArvore(null, 51, null);
+            MinhaArvore Arvore38 = new MinhaArvore(null, 38, null);
+            MinhaArvore Arvore79 = new MinhaArvore(null, 79, null);
+            MinhaArvore Arvore1 = new MinhaArvore(null, 1, null);
+            MinhaArvore Arvore235 = new MinhaArvore(null, 235, null);
+            MinhaArvore Arvore121 = new MinhaArvore(null, 121, null);
+            MinhaArvore Arvore223 = new MinhaArvore(null, 223, null);
+            MinhaArvore Arvore161 = new MinhaArvore(null, 161, null);
+        }
+    }
+}
